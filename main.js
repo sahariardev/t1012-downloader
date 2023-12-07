33b63d62c58810abc7c0ce8c41baf5252c9959c8
sahariardev <rifatsahariar@gmail.com>
2023-12-07 21:14:25 +0600

    Added connection form

--- main.js
+++ main.js
@@ -3,20 +3,23 @@ const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    maximizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+        devTools: true,
+        nodeIntegration: true,
+        webviewTag: true,
+        contextIsolation: false,
+        preload: path.join(__dirname, 'preload.js'),
     }
-  })
+});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+mainWindow.setMenuBarVisibility(false);
+mainWindow.setResizable(false);
+mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
