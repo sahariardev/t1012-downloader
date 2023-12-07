2089f0cdeae4a396ad726c79655c521f57c8256b
sahariardev <rifatsahariar@gmail.com>
2023-12-07 22:02:42 +0600

    Added functions to save connection form

--- renderer.js
+++ renderer.js
@@ -1,7 +1,39 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+
+window.rifat = 'Hello world';
+window.$ = window.jQuery = require('./js/jquery-3.7.1.js'); 
+
+const LOCAL_STORAGE_CACHE_KEY = 'connection';
+      
+$(function(){
+    $('#submit-btn').on('click', function() {
+      
+        const connection = {
+            name : $('#connection-name').val(),
+            hostName : $('#host-name').val(),
+            hostPort : $('#host-port').val(),
+            userName: $('#user-name').val(),
+            password: $('#password').val(),
+            keyPath : $('#key-path').val(),
+            jumpHost : $('#jump-host').val(),
+            jumpPort: $('#jump-port').val(),
+            jumpHostPassword: $('#jump-host-password').val(),
+            jumpHostKeyPath: $('#jump-host-key-path').val(),
+            jumpHostHomePath: $('#jump-host-home-path').val()
+        };
+        
+        saveConnection(connection);
+
+    });
+  });
+
+  const getConnections = () => {
+    const connectionStr = localStorage.getItem(LOCAL_STORAGE_CACHE_KEY) || '[]';
+
+    return JSON.parse(connectionStr);
+  }
+
+  const saveConnection = (connection) => {
+    let connections = getConnections();
+    connections.push(connection);
+    localStorage.setItem(LOCAL_STORAGE_CACHE_KEY, JSON.stringify(connections));
+  }
